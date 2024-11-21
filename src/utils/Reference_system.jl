f5329f7aa4936d0675abbab760b4bf9ff6824e5a
Nihal Simha <nihal.simha19@imperial.ac.uk>
2024-11-21 00:37:15 -0500

    Fixed bugs with aerobraking phase indices and added direction to callback events

--- src/utils/Reference_system.jl
+++ src/utils/Reference_system.jl
@@ -59,7 +59,7 @@ function rvtoorbitalelement(r, v, m, planet)
     i_z = [0, 0, 1]
 
     Energy = sqrt(dot(v, v)/2 - planet.μ/dot(r, r))
-    a = - planet.μ / (2 * Energy)
+    a = -planet.μ / (2 * Energy)
     h = cross(r, v)
     index = 0
 
